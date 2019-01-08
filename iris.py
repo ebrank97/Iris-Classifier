9a6f288e36e170355bb9971e99903a645b8b8894
Ebran Khan <ebrank97@gmail.com>
2019-01-08 15:23:26 +0530

    Modeling Different Kernel Svm classifier using Iris Petal features

--- iris.py
+++ iris.py
@@ -12,6 +12,7 @@ iris = datasets.load_iris()
 
 
 #Modeling Different Kernel Svm classifier using Iris Sepal features
+'''
 X = iris.data[:, :2]
 y = iris.target
 C = 1.0
@@ -30,10 +31,34 @@ print('Training Accuracy LinearSVC: ', lin_svc.score(X, y))
 print('Training Accuracy RBF: ', rbf_svc.score(X, y))
 print('Training Accuracy PolynomialSVC: ', poly_svc.score(X, y))
 
-print('Output LinearKernel: ', svc.predict([[5.2,  3.5]]))
-print('Output LinearSVC: ', lin_svc.predict([[5.2,  3.5]]))
-print('Output RBF: ', rbf_svc.predict([[5.2,  3.5]]))
-print('Output SVC: ', poly_svc.predict([[5.2,  3.5]]))
+print('Prediction results LinearKernel: ', svc.predict([[5.2,  3.5]]))
+print('Prediction results LinearSVC: ', lin_svc.predict([[5.2,  3.5]]))
+print('Prediction results RBF: ', rbf_svc.predict([[5.2,  3.5]]))
+print('Prediction results SVC: ', poly_svc.predict([[5.2,  3.5]]))
+'''
+#Modeling Different Kernel Svm classifier using Iris Petal features
+X = iris.data[:, 2:]
+y = iris.target
+C = 1.0
+
+# SVC with linear kernel
+svc = svm.SVC(kernel='linear', C=C).fit(X, y)
+# LinearSVC (linear kernel)
+lin_svc = svm.LinearSVC(C=C).fit(X, y)
+# SVC with RBF kernel
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
+# SVC with polynomial (degree 3) kernel
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
+
+print('Training Accuracy LinearKernel: ', svc.score(X, y))
+print('Training Accuracy LinearSVC: ', lin_svc.score(X, y))
+print('Training Accuracy RBF: ', rbf_svc.score(X, y))
+print('Training Accuracy PolynomialSVC: ', poly_svc.score(X, y))
+
+print('Prediction results LinearKernel: ', svc.predict([[2.4,  1.2]]))
+print('Prediction results LinearSVC: ', lin_svc.predict([[2.4,  1.2]]))
+print('Prediction results RBF: ', rbf_svc.predict([[2.4,  1.2]]))
+print('Prediction results SVC: ', poly_svc.predict([[2.4,  1.2]]))
 
 #Modeling Svm classifier using Iris Sepal and Petal features
 '''
